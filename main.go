69c2ebee2f7f15a2a526b42468e11c2e78d525f0
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-10-11 18:14:28 +0900

    update

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/tzmfreedom/gocui"
 )
 
+var cx int = 0
+var cy int = 0
+
 func main() {
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
@@ -16,41 +19,119 @@ func main() {
 	}
 	defer g.Close()
 
-	g.Cursor = true
 	g.SetManagerFunc(layout)
+	g.Mouse = true
 
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		panic(err)
 	}
+	if err := g.SetKeybinding("", 'q', gocui.ModNone, quit); err != nil {
+		panic(err)
+	}
+	up := func (g *gocui.Gui, v *gocui.View) error {
+		if v != nil {
+			if cy != 0 {
+				cy--
+			}
+			v.SetCursor(0, cy+1)
+		}
+		return nil
+	}
+
+	down := func (g *gocui.Gui, v *gocui.View) error {
+		if v != nil {
+			if cy <= maxRecordLength {
+				cy++
+				v.SetCursor(0, cy+1)
+			}
+		}
+		return nil
+	}
+
+	setCurrentView := func (g *gocui.Gui, v *gocui.View) error {
+		if _, err := g.SetCurrentView(v.Name()); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := g.SetKeybinding("Menu", 'k', gocui.ModNone, up); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Menu", 'j', gocui.ModNone, down); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Records", gocui.KeyArrowUp, gocui.ModNone, up); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Records", 'k', gocui.ModNone, up); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Records", gocui.KeyArrowDown, gocui.ModNone, down); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Records", 'j', gocui.ModNone, down); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Menu", gocui.MouseLeft, gocui.ModNone, setCurrentView); err != nil {
+		panic(err)
+	}
+	if err := g.SetKeybinding("Records", gocui.MouseLeft, gocui.ModNone, setCurrentView); err != nil {
+		panic(err)
+	}
+
+	client := soapforce.NewClient()
+	username := os.Getenv("SALESFORCE_USERNAME")
+	password := os.Getenv("SALESFORCE_PASSWORD")
+	result, err := client.Login(username, password)
+	userInfo = result.UserInfo
+	qr, err := client.Query("SELECT Id, LastName, FirstName FROM Contact")
+	if err != nil {
+		panic(err)
+	}
+	records = qr.Records
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		panic(err)
 	}
-
 }
 
+var client *soapforce.Client
+var records []*soapforce.SObject
+var userInfo *soapforce.GetUserInfoResult
+var maxRecordLength = 20
+
 func layout(g *gocui.Gui) error {
-	client := soapforce.NewClient()
-	username := os.Getenv("SALESFORCE_USERNAME")
-	password := os.Getenv("SALESFORCE_PASSWORD")
-	u, err := client.Login(username, password)
-	if err != nil {
-		return err
-	}
 
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("User Info", 0, 0, maxX/2, maxY/2); err != nil {
+	if v, err := g.SetView("Menu", 0, 0, 20, 5); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		info := u.UserInfo
+		menues := []string{
+			"Execute Query",
+			"Describe SObject",
+			"Execute Anonymous",
+		}
+		for _, menu := range menues {
+			fmt.Fprintln(v, menu)
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		v.SetCursor(0, 0)
+	}
+	if v, err := g.SetView("User Info", maxX/2+1, 0, maxX-1, 10); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
 		fields := map[string]string{
-			"ID":       info.UserId,
-			"Name":     info.UserName,
-			"FullName": info.UserFullName,
-			"Email":    info.UserEmail,
-			"OrgID":    info.OrganizationId,
-			"OrgName":  info.OrganizationName,
+			"ID":       userInfo.UserId,
+			"Name":     userInfo.UserName,
+			"FullName": userInfo.UserFullName,
+			"Email":    userInfo.UserEmail,
+			"OrgID":    userInfo.OrganizationId,
+			"OrgName":  userInfo.OrganizationName,
 		}
 		max := 0
 		for k, _ := range fields {
@@ -64,10 +145,6 @@ func layout(g *gocui.Gui) error {
 		}
 	}
 
-	qr, err := client.Query("SELECT Id, LastName, FirstName FROM Contact")
-	if err != nil {
-		return err
-	}
 	if v, err := g.SetView("Records", 0, maxY/2+1, maxX-1, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
@@ -78,20 +155,20 @@ func layout(g *gocui.Gui) error {
 		for _, h := range headers {
 			fmt.Fprintln(v, h)
 		}
-		for i, r := range qr.Records {
-			v.Highlight = true
-			v.SelBgColor = gocui.ColorGreen
-			v.SelFgColor = gocui.ColorBlack
-			v.SetCursor(0, 1)
-			fmt.Fprintln(v, "aaa bbb ccc")
+		for i, r := range records {
 			for _, h := range headers {
 				fmt.Fprintln(v, r.Fields[h])
 			}
-			if i > 5 {
+			if i > maxRecordLength {
 				break
 			}
 		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		v.SetCursor(0, 1)
 	}
+	g.SetCurrentView("Menu")
 	return nil
 }
 
